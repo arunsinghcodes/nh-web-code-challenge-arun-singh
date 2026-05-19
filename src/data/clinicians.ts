f6b0cfe173ce93b170194d1ab14c1a108eefaa01
Arun Singh <arunsinghsnd@gmail.com>
2026-05-19 12:32:19 +0530

    feat: add clinicians and labs static data

---
+++ src/data/clinicians.ts
@@ -0,0 +1,39 @@
+import type { Clinician } from "../types";
+
+export const clinicians: Clinician[] = [
+  {
+    id: 1,
+    name: "Barb",
+    address: "4120 Garfield Ave, Minneapolis, MN 55409",
+  },
+  {
+    id: 2,
+    name: "Isaac",
+    address: "140 104th Ln NW, Blaine MN 55448",
+  },
+  {
+    id: 3,
+    name: "Marisol",
+    address: "2393 Kalmia Ave, Boulder, CO 80304",
+  },
+  {
+    id: 4,
+    name: "Mary",
+    address: "608 Spruce Dr, Hudson, WI 54016",
+  },
+  {
+    id: 5,
+    name: "Shawna",
+    address: "1727 W Highland Pkwy, St Paul, MN 55116",
+  },
+  {
+    id: 6,
+    name: "Shelly",
+    address: "1232 3rd St, Hudson, WI 54016",
+  },
+  {
+    id: 7,
+    name: "Tom",
+    address: "14173 Flagstone Trail, Apple Valley MN 55124",
+  },
+];
---
+++ src/data/labs.ts
@@ -0,0 +1,29 @@
+import type { Lab } from "../types";
+
+export const labs: Lab[] = [
+  {
+    id: 1,
+    name: "Edina Lab",
+    address: "6525 France Ave, Edina, MN, 55435",
+  },
+  {
+    id: 2,
+    name: "Medical Arts Lab",
+    address: "835 Nicollet Mall, Minneapolis, MN 55402",
+  },
+  {
+    id: 3,
+    name: "Bloomington Lab",
+    address: "2716 E 82nd St, Bloomington, MN 55425",
+  },
+  {
+    id: 4,
+    name: "Hudson Lab",
+    address: "400 2nd St S, Hudson, WI 54016",
+  },
+  {
+    id: 5,
+    name: "Boulder Lab",
+    address: "4750 Nautilus Ct S, Boulder, CO 80301",
+  },
+];
